770a0a4ac1da9bfc1ff72f69b5e67ca3f0170282
Jeremy reed <jeremyreed1542@gmail.com>
2026-08-12 22:02:43 +0000

    Touched up structure; improve input validation next

--- StudentScores_Analyzer/Source.cpp
+++ StudentScores_Analyzer/Source.cpp
@@ -1,82 +1,87 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-using namespace std;
+#include <limits>
 
 
-const int NUM_TESTS = 3, MAX_STUDENTS = 100;
+const int NUM_TESTS = 3;
+const int MAX_STUDENTS = 100;
+const int MAX_SCORE = 100;
 
 
 void findAverage(const int[][NUM_TESTS], double[], int);
 
-void displayList(string[], const double[], const int);
+void displayList(std::string[], const double[], int);
 
-void selectionSort(string[], double averages[], const int size);
+void selectionSort(std::string[], double[], int);
 
 
 int main()
 {
-	string studentNames[MAX_STUDENTS];
+	std::string studentNames[MAX_STUDENTS];
 	int studentTests[MAX_STUDENTS][NUM_TESTS];
 	double averages[MAX_STUDENTS];
 	
 	int count;
-	int students1;
+	int studentCount;
 	
 
 
 	
 	do 
 	{
-	cout << "This program is used to average students test scores for 3 tests up to a maximum of 100 students.\n";
+	std::cout << "This program is used to average students test scores for 3 tests up to a maximum of 100 students.\n";
 	
 	
-	cout << "How many students are there?";
-	cin >> students1;
+	std::cout << "How many students are there?";
+	std::cin >> studentCount;
 
 
-	if (students1 > 100 || students1 < 1)
+	if (studentCount > MAX_STUDENTS || studentCount < 1)
 
 	{
-		cout << "Invalid input please enter a number between 1-100.\n";
+		std::cout << "Invalid input please enter a number between 1-100.\n";
 
 	}
 	
-	} while (students1 > 100 || students1 < 1);
+	} while (studentCount > MAX_STUDENTS || studentCount < 1);
 		
 
-	for (count = 0; count < students1; count++)
+	for (count = 0; count < studentCount; count++)
 	{
-		cout << "Enter the name of student #" << (count + 1) << ": ";
-		cin.ignore();
-		getline(cin, studentNames[count]);
-		cout << studentNames[count] << "\n";
+		std::cout << "Enter the name of student #" << (count + 1) << ": ";
+
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::getline(std::cin, studentNames[count]);
 
 
-		do
-		{
-			cout << "Enter the score [1-100] for test #"
+		for (int index = 0; index < NUM_TESTS; index++)
+    	{
+			do
+			{
+				std::cout << "Enter the score [0-100] for test #"
 				<< (index + 1) << " of student "
 				<< studentNames[count] << ": ";
-			cin >> studentTests[count][index];
+				std::cin >> studentTests[count][index];
 
-			if (studentTests[count][index] < 1 ||
-				studentTests[count][index] > 100)
-			{
-				cout << "Invalid score. Enter a value between 1 and 100.\n";
-			}
+				if (studentTests[count][index] < 0 ||
+				studentTests[count][index] > MAX_SCORE)
+				{
+					std::cout << "Invalid score. Enter a value between 0 and 100.\n";
+				}
 
-		} while (studentTests[count][index] < 1 ||
-				studentTests[count][index] > 100);
+			} while (studentTests[count][index] < 0 ||
+					studentTests[count][index] > MAX_SCORE);
+		}
 	}
 
 
-	findAverage(studentTests, averages, students1);
+	findAverage(studentTests, averages, studentCount);
 	
 	
-	selectionSort(studentNames, averages, students1);
+	selectionSort(studentNames, averages, studentCount);
 
-	displayList(studentNames, averages, students1);
+	displayList(studentNames, averages, studentCount);
 
 	
 
@@ -85,7 +90,7 @@ int main()
 
 
 
-void findAverage(const int tests1[][tests], double average[], int size)
+void findAverage(const int tests1[][NUM_TESTS], double average[], int size)
 {
 	int student, test;
 	double total = 0;
@@ -94,35 +99,31 @@ void findAverage(const int tests1[][tests], double average[], int size)
 	for (student = 0; student < size; student++)
 	{
 		total = 0;
-		for (test = 0; test < tests; test++)
+		for (test = 0; test < NUM_TESTS; test++)
 		{
 			
 			total += tests1[student][test];
 			
-			average[student] = total / tests;
-
-			
 		}
-		
-		
+
+		average[student] = total / NUM_TESTS;
 	}
 
 }
 
 
-void displayList(string names[], const double averages[], const int size)
+void displayList(std::string names[], const double averages[], const int size)
 {
 	
-	cout << "These are the tests scores after sorting them highest to lowest.\n";
-
+	std::cout << "Students sorted by average score, highest to lowest:\n";
 
 	int student;
+
+	std::cout << std::fixed << std::setprecision(2);
+
 	for (student = 0; student < size; student++)
 	{
-		cout << fixed << showpoint << setprecision(2);
-		
-
-		cout << (student + 1) << ". " << names[student]  << setw(8) << "--> " << averages[student] << "\n";
+		std::cout << (student + 1) << ". " << names[student]  << std::setw(8) << "--> " << averages[student] << "\n";
 	}
 
 
@@ -130,17 +131,17 @@ void displayList(string names[], const double averages[], const int size)
 
 
 // Sorts students by average grades
-void selectionSort(string names[], double averages[], const int size)
+void selectionSort(std::string names[], double averages[], const int size)
 {
 	int start, maxIndex; 
 	double maxValue;
-	string maxValue1;
+	std::string maxName;
 
 	for (start = 0; start < (size - 1); start++)
 	{
 		maxIndex = start;
 		maxValue = averages[start];
-		maxValue1 = names[start];
+		maxName = names[start];
 		
 		
 		for (int index = start + 1; index < size; index++)
@@ -148,7 +149,7 @@ void selectionSort(string names[], double averages[], const int size)
 			if (averages[index] > maxValue)
 			{
 				maxValue = averages[index];
-				maxValue1 = names[index];
+				maxName = names[index];
 				maxIndex = index;
 				
 			}
@@ -156,7 +157,7 @@ void selectionSort(string names[], double averages[], const int size)
 		averages[maxIndex] = averages[start];
 		averages[start] = maxValue;
 		names[maxIndex] = names[start];
-		names[start] = maxValue1;
+		names[start] = maxName;
 	}
 
 }
